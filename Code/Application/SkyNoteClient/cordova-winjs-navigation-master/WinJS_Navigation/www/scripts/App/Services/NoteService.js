06266cf50cb10978cd1a135a145e14321a14d056
Joanna Grzonkowska <joannagrzonkowska@gmail.com>
2015-11-13 19:27:14 +0100

    #1

--- Code/Application/SkyNoteClient/cordova-winjs-navigation-master/WinJS_Navigation/www/scripts/App/Services/NoteService.js
+++ Code/Application/SkyNoteClient/cordova-winjs-navigation-master/WinJS_Navigation/www/scripts/App/Services/NoteService.js
@@ -7,33 +7,33 @@ var NoteService = function (urls) {
         
         $.ajaxSetup({ cache: false });
 
-        //$.ajax({
-        //    url: 'http://localhost:59284/api/note',
-        //    type: 'GET',
-        //    success: function (data) {
-        //        var mappedData = $.map(data, function (item) {
-        //            return new NoteModel(item);
-        //        });
-        //        handler(mappedData);
-        //    }
-        //});
+        $.ajax({
+            url: 'http://localhost:59284/api/note',
+            type: 'GET',
+            success: function (data) {
+                var mappedData = $.map(data, function (item) {
+                    return new NoteModel(item);
+                });
+                handler(mappedData);
+            }
+        });
     };
 
     self.addNote = function (note, handler) {
          $.ajaxSetup({ cache: false });
 
-         //$.ajax({
-         //    url: 'http://localhost:59284/api/note',
-         //    type: 'POST',
-         //    data: note,
-         //    success: function (data) {
-         //        if (data.IsSuccess) {
-         //            $("#success-message").html("OK").show();
-         //            handler();
-         //        } else {
-         //            $(".alert").html(data.ErrorsToString).show();
-         //        }
-         //    }
-         //});
+         $.ajax({
+             url: 'http://localhost:59284/api/note',
+             type: 'POST',
+             data: note,
+             success: function (data) {
+                 if (data.IsSuccess) {
+                     //$("#success-message").html("OK").show();
+                     handler();
+                 } else {
+                     //$(".alert").html(data.ErrorsToString).show();
+                 }
+             }
+         });
     }
 };

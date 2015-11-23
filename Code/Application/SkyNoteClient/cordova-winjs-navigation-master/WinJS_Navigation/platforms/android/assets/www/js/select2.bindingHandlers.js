6900895bf9e892ae5f4e6e77ca9ee6dc9ff487bc
Joanna Grzonkowska <joannagrzonkowska@gmail.com>
2015-11-23 21:32:31 +0100

    dropdown list - select 2

---
+++ Code/Application/SkyNoteClient/cordova-winjs-navigation-master/WinJS_Navigation/platforms/android/assets/www/js/select2.bindingHandlers.js
@@ -0,0 +1,8 @@
+﻿ko.bindingHandlers.select2 = {
+    init: function (element, valueAccessor) {
+        var options = ko.toJS(valueAccessor()) || {};
+        setTimeout(function () {
+            $(element).select2(options);
+        }, 0);
+    }
+};

9c5339cf5406e7ff5668a974ef5d1de2ecf2d1c8
Joanna Grzonkowska <joannagrzonkowska@gmail.com>
2015-12-04 20:39:04 +0100

    #1

---
+++ Code/Application/SkyNote/SkyNote/ViewModels/NoteDetailsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CQRS.Implementation.Models;
+using System.Collections.Generic;
+
+namespace SkyNote.ViewModels
+{
+    public class NoteDetailsViewModel
+    {
+        public IEnumerable<CategoryDTO> Categories { get; set; }
+        public NoteDTO Note { get; set; }
+    }
+}

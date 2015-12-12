0ff9c93e37670dd1578fcf0facf3398fd1db9ccf
Joanna Grzonkowska <joannagrzonkowska@gmail.com>
2015-12-12 23:37:48 +0100

    cleaning up repo

---
+++ Prototypes/ServerWithServices/BusinessLogic/Services/INotesService.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLogic.Models;
+using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services
+{
+    public interface INotesService
+    {
+        CommandResult Add(NoteModel examinationMessage, ref int examinationId);
+    }
+}
---
+++ Prototypes/ServerWithServices/Common/CommandResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class CommandResult
+    {
+        public CommandResult(IEnumerable<string> errors = null)
+        {
+            Errors = errors == null ? new List<string>() : errors.ToList();
+            IsSuccess = !Errors.Any();
+        }
+
+        public bool IsSuccess { get; private set; }
+
+        public IEnumerable<string> Errors { get; private set; }
+
+        public string ErrorsToString { get {
+            return string.Join(",", Errors);
+        } }
+    }
+}
